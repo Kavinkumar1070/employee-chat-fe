644abc152fcdeb0866b2a709f18c1df11dad557a
kavinconverse <121674791+Kavinkumar1070@users.noreply.github.com>
2024-09-26 18:54:05 +0530

    web server adjusted

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route to serve the HTML page
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/chatbot.html')
+def chatbot():
+    return render_template('chatbot.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/onboard.html')
+def onboard():
+    return render_template('onboard.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

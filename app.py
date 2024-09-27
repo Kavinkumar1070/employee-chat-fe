5c763de22353545484573a9a0205d9d22be3ce06
Hariprathap-converse <hariprathap@conversedatasolutions.com>
2024-09-27 13:02:05 +0530

    Updated PORT

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def onboard():
     return render_template('onboard.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT env variable or 5000 by default
+    app.run(host="0.0.0.0", port=port)

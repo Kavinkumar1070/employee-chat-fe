68c2743e8be8199a7f839c4cd83fcd85dacec6f6
Hariprathap-converse <hariprathap@conversedatasolutions.com>
2024-09-27 14:13:51 +0530

    add gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\conve\ZA_Project\success\employee-chat-fe\venv
+set VIRTUAL_ENV=C:\Users\Converse\OneDrive - CONVERSE Data Solutions\converse-chat-bot-fe\employee-chat-fe\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
